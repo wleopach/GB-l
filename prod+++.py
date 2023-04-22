a8c0d9de466db1a249840936f0d1d4c01e213c14
wleopach <wleonardop@gmail.com>
2023-04-22 11:25:36 -0400

    prod+++

--- prod+++.py
+++ prod+++.py
@@ -1,4 +1,5 @@
 from data_reader import load_dicts
+from collections import defaultdict
 import psutil
 import pickle
 from tqdm import tqdm
@@ -14,7 +15,7 @@ cod_ = ['ID_CUSTOMER', 'DATE', 'TELEPHONE', 'CALIFICACION', 'DESCRIPTION_COD1', 
 gestion = {key: value['GESTION'][cod_] for key, value in prod.items() if 'GESTION' in value}
 cc_g = [set(value['ID_CUSTOMER']) for value in gestion.values()]
 tuples1 = [(key, row, 'GESTION')
-           for key, value in tqdm(gestion.items())
+           for key, value in tqdm(gestion.items(), colour="green")
            for index, row in value.iterrows() if row['ID_CUSTOMER'] != 'nan']
 
 act_ = ['ID_CUSTOMER', 'DATE', 'CUSTOMER_PHONE', 'CUSTOMER_CHARS', 'DESCRIPTION_COD_ACT']
@@ -23,7 +24,7 @@ gestion_wp = {key: value['GESTION_WHATSAPP'][act_] for key, value in prod.items(
 cc_gw = [set(value['ID_CUSTOMER']) for value in gestion_wp.values()]
 ccw = set.union(*cc_gw)
 tuples2 = [(key, row, 'GESTION_WHATSAPP')
-           for key, value in tqdm(gestion_wp.items())
+           for key, value in tqdm(gestion_wp.items(), colour="green")
            for index, row in value.iterrows() if row['ID_CUSTOMER'] != 'nan']
 
 new_dict = {}
@@ -37,30 +38,43 @@ cols = {'GESTION': cod_[1:],
 @ray.remote
 def process_customer(t: tuple):
     row = t[1]
-    new_dict.setdefault(row['ID_CUSTOMER'], dict())
+    customer_id = row['ID_CUSTOMER']
     key = t[0]
-    new_dict[row['ID_CUSTOMER']].setdefault((key[0], key[1], t[2]), set())
-    new_dict[row['ID_CUSTOMER']][(key[0], key[1], t[2])].add(tuple(row[cols[t[2]]]))
-    return  new_dict
+    customer_info = tuple(row[cols[t[2]]])
+    return {(customer_id, key, t[2]): {customer_info}}
 
 
 num_cpus = psutil.cpu_count(logical=True)
 ray.init(num_cpus=int(num_cpus), ignore_reinit_error=True, include_dashboard=False)
-for t in tqdm(tuples):
-    process_customer.remote(t)
+refs = [process_customer.remote(tu) for tu in tqdm(tuples, colour="yellow")]
+pis = ray.get(refs)
+pis = [a for a in pis if a if a!= "error"]
 print('Wrapping results')
-Diccionario_Com = new_dict.copy()
+# Create a defaultdict to store the merged values
+merged_dict = defaultdict(set)
+
+# Iterate over each dictionary in the list
+for d in tqdm(pis,colour= "red"):
+    # Iterate over each key-value pair in the dictionary
+    for k, v in d.items():
+        # Update the merged_dict with the set union of the current value and the existing value for the key
+        merged_dict[k] |= v
+
+# Convert the defaultdict back to a regular dictionary
+merged_dict = dict(merged_dict)
 gc.collect()
 while ray.is_initialized():
     ray.shutdown()
 print("Ray_Seguro")
 
-
+for k, v in tqdm(merged_dict.items(), colour="red"):
+    new_dict.setdefault(k[0], dict())
+    new_dict[k[0]][k[1:]] = v
 # Get a random key-value pair
-random_key, random_value = random.choice(list(Diccionario_Com.items()))
+random_key, random_value = random.choice(list(new_dict.items()))
 
 print("Random key:", random_key)
 print("Random value:", random_value)
 print('writing the results')
 with open("data/outputs/Productividad_Nuevo" + ".pickle", 'wb') as handle:
-    pickle.dump(Diccionario_Com, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    pickle.dump(new_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
